a87f95115af1e69b544af6d049e503cfc9a5abae
moonix0 <moongraph21@gmail.com>
2023-05-06 12:31:10 +0700

    Improve login and register

--- register.js
+++ register.js
@@ -1,35 +1,38 @@
-let formRegister = document.getElementById("form-register");
-let fullName = document.getElementById("fullname");
-let newEmail = document.getElementById("email-register");
-let newPass = document.getElementById("password-register");
-let users = JSON.parse(localStorage.getItem("users"));
+let formRegister = document.getElementById('form-register');
+let fullName = document.getElementById('fullname');
+let newEmail = document.getElementById('email-register');
+let newPass = document.getElementById('password-register');
+let users = JSON.parse(localStorage.getItem('users'));
+let warningText = document.querySelector('#warning');
+const apiAuth = 'https://643ea9d8c72fda4a0bfd69ad.mockapi.io/api/v1';
 
-formRegister.addEventListener("submit", (e) => {
+formRegister.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (fullName.value === "" || newEmail === "" || newPass === "") {
-    alert("isi semua field");
-    return false;
-  }
-
-  let allUsers = {
-    fullName: fullName.value,
-    emailUser: newEmail.value,
-    passwordUser: newPass.value,
-  };
-
-  let userRegister = [];
-
-  if (localStorage.getItem("users") === null) {
-    userRegister.push(allUsers);
-    localStorage.setItem("users", JSON.stringify(userRegister));
+  if (fullName.value === '' || newEmail.value === '' || newPass.value === '') {
+    warningText.innerHTML = 'isi semua field!';
   } else {
-    userRegister = JSON.parse(localStorage.getItem("users"));
-    userRegister.push(allUsers);
-    localStorage.setItem("users", JSON.stringify(userRegister));
+    let allUsers = {
+      username: fullName.value,
+      email: newEmail.value,
+      password: newPass.value,
+    };
+
+    const registerUser = async (apiUrl) => {
+      await fetch(`${apiUrl}/users`, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(allUsers),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          alert('Register berhasil');
+          location.replace('./login.html');
+        })
+        .catch((e) => console.error(e));
+    };
+
+    registerUser(apiAuth);
   }
-
-  console.log(userRegister);
-
-  alert("Register berhasil");
 });
